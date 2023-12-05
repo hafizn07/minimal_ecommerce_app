0b5ef2703db2f8fe5ef70a23b3fd7367419a7f88
hafizn07 <hafiznjh007@gmail.com>
2023-12-05 13:04:26 +0530

    chore: Initialized firebase

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,14 @@ import 'package:minimal_ecommerce_app/pages/shop_page.dart';
 import 'package:minimal_ecommerce_app/themes/theme.dart';
 import 'package:minimal_ecommerce_app/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
